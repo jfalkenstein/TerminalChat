4d90c45452074d32e24fbbf03209aa414e1e6813
Jonathan Falkenstein <jtfalkenstein@gmail.com>
2022-10-16 11:50:29 -0500

    initial app stub

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,59 @@
-use nokhwa::{Camera, CameraFormat};
-use image::{ImageBuffer, Rgb, Pixel};
+mod ascii_frame;
+
+
+use const_format::formatcp;
 use image::imageops::{resize, FilterType};
+use image::{ImageBuffer, Pixel, Rgb};
+use nokhwa::{Camera, CameraFormat};
+use tui::layout::Rect;
+use tui::text::Text;
 use std::io::{self, Write};
-use colored::*;
+use crate::ascii_frame::{AsciiFrame};
+use tui::{
+    backend::CrosstermBackend,
+    widgets::{Widget, Block, Borders, Paragraph},
+    layout::{Layout, Constraint, Direction},
+    Terminal
+};
+use crossterm::{
+    event::{self, DisableMouseCapture, EnableMouseCapture, Event, KeyCode},
+    execute,
+    terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
+};
 
 
-const GRADIENT: &'static str = "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\\|()1{}[]?-_+~<>i!lI;:,\"^`'. ";
 const STANDARD_WIDTH: u32 = 640;
 const STANDARD_HEIGHT: u32 = 480;
-const TERMINAL_WIDTH: u32 = 80;
-const TERMINAL_HEGITH: u32 = (TERMINAL_WIDTH * STANDARD_HEIGHT) / STANDARD_WIDTH;
+const TERMINAL_WIDTH: u32 = 120;
+// Ascii text is twice as tall as it is wide, so we shink it double
+const TERMINAL_HEIGHT: u32 = (TERMINAL_WIDTH * STANDARD_HEIGHT) / STANDARD_WIDTH / 2;
+const FRAME_RATE: u32 = 15;
+const RESET_FRAME_CODE: &str = formatcp!("{esc}[2J{esc}[1;1H", esc = 27 as char);
 
 fn main() {
+    // enable_raw_mode().unwrap();
+    let mut stdout = io::stdout();
+    // execute!(stdout, EnterAlternateScreen, EnableMouseCapture).unwrap();
+    // let backend = CrosstermBackend::new(stdout);
+    // let mut terminal = Terminal::new(backend).unwrap();
+    
     let mut camera = get_camera();
     camera.open_stream().unwrap();
     loop {
         match get_next_frame(&mut camera) {
             None => {
                 println!("Skipped!")
-            },
+            }
             Some(frame) => {
-                let ascii_text = as_ascii(frame);
-                io::stdout().write_all(ascii_text.as_bytes()).unwrap();
-                print!("{esc}[2J{esc}[1;1H", esc = 27 as char);
+                let ascii_frame = AsciiFrame::from(frame);
+                let ascii_string = ascii_frame.to_string();
+                // terminal.draw(|f|{
+                //     let widget = Paragraph::new(Text::raw(ascii_string));
+                //     f.render_widget(widget, f.size())
+                // }).unwrap();
+                // terminal.clear().unwrap();
+                stdout.write_all(ascii_string.as_bytes()).unwrap();
+                stdout.write_all(RESET_FRAME_CODE.as_bytes()).unwrap();
             }
         };
     }
@@ -31,40 +62,22 @@ fn main() {
 fn get_camera() -> Camera {
     let mut camera = Camera::new(
         0,
-        Some(CameraFormat::new_from(STANDARD_WIDTH, STANDARD_HEIGHT, nokhwa::FrameFormat::MJPEG, 5))
-    ).unwrap();
+        Some(CameraFormat::new_from(
+            STANDARD_WIDTH,
+            STANDARD_HEIGHT,
+            nokhwa::FrameFormat::MJPEG,
+            FRAME_RATE,
+        )),
+    )
+    .unwrap();
     camera
 }
 
 fn get_next_frame(camera: &mut Camera) -> Option<ImageBuffer<Rgb<u8>, Vec<u8>>> {
     let frame = camera.frame().unwrap();
-    let new = ImageBuffer::from_vec(
-        frame.width(),
-        frame.height(),
-        frame.into_vec()
-    )?;
+    let new = ImageBuffer::from_vec(frame.width(), frame.height(), frame.into_vec())?;
 
-    let resized: ImageBuffer<Rgb<u8>, Vec<u8>> = resize(&new, TERMINAL_WIDTH, TERMINAL_HEGITH, FilterType::Nearest);
+    let resized: ImageBuffer<Rgb<u8>, Vec<u8>> =
+        resize(&new, TERMINAL_WIDTH, TERMINAL_HEIGHT, FilterType::Nearest);
     return Some(resized);
-} 
-
-fn as_ascii(frame: ImageBuffer<Rgb<u8>, Vec<u8>>) -> String {
-    let mut ascii_frame = String::new();
-    for line in frame.rows() {
-        for pixel in line.into_iter(){
-            let ascii_pixel = map_pixel(pixel);
-            ascii_frame.push_str(ascii_pixel.as_str());
-        }
-        ascii_frame.push('\n');
-    }
-    return ascii_frame;
-}
-
-fn map_pixel(pixel: &Rgb<u8>) -> String {
-    let intensity = usize::from(pixel.0[0]) + usize::from(pixel.0[1]) + usize::from(pixel.0[2]);
-    let gradient_length = GRADIENT.len();
-    let index = (intensity * (gradient_length - 1)) / (255 * 3);
-    let character = GRADIENT.chars().collect::<Vec<char>>()[index];
-    let colorized = character.to_string().truecolor(pixel.0[0], pixel.0[1], pixel.0[2]);
-    return colorized.to_string();
 }
